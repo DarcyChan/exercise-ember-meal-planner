d873d611bb7fe874816978ed7d0c93f13bd05d2f
Darcy Chan <darcy.chan@atmist.com>
2017-09-06 15:24:16 -0600

    Testing some production settings.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,14 +4,7 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-    let app = new EmberApp(defaults, {
-        SRI: {
-            crossorigin: 'anonymous'
-        },
-        fingerprint: {
-            prepend: 'https://subdomain.cloudfront.net/'
-        }
-    });
+    let app = new EmberApp(defaults, {});
 
     // Use `app.import` to add additional libraries to the generated
     // output files.
